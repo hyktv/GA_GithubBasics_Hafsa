0c3fb2aaa4fd1dbea8d08b26001654bd54277745
hyktv <hafsa.yktv@gmail.com>
2024-01-19 21:29:43 -0800

    added another console writeline

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Professor!");
+            Console.WriteLine("My name is Hafsa Mohamed and i want to build islamic apps!");
         }
     }
 
