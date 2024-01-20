0a53bd26a15820a7bf7502bf65ecb2e84cd41846
hyktv <hafsa.yktv@gmail.com>
2024-01-19 21:27:49 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GA_GithubBasics_Hafsa 
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Professor!");
+        }
+    }
+
+}
